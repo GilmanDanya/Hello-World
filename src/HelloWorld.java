7e9563afbf1df83a40d303d37e749d5a6a519c00
Danil Gilman <Gilman.DV@ya.ru>
2022-09-20 23:08:23 +0300

    Add new strings

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Worl");
+        System.out.println("Hello Wor");
+        System.out.println("Hello Wo");
+        System.out.println("Hello W");
     }
 }
